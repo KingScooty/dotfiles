20c2a6df6a2b1ae56b6296fb23040689e9a9d7f9
Scotty Vernon <scott.vernon@gmail.com>
2016-04-30 18:02:04 +0100

    Remove unecessary packages.

--- brew.sh
+++ brew.sh
@@ -20,6 +20,7 @@ brew install findutils --with-default-names
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 brew install gnu-which --with-default-names
+
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
@@ -77,26 +78,18 @@ brew install pngcheck
 # brew install xz
 
 # Install other useful binaries.
-brew install ack
+brew install ack #faster than grep
 brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-# brew install rhino
+
+# brew install pv
 brew install speedtest_cli
 brew install ssh-copy-id #installs public key on remote via ssh
-brew install testssl
 brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
+brew install webkit2png #screenshot webpage
 
 # Remove outdated versions from the cellar.
 brew cleanup
