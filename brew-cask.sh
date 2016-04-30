36ba26e835e3cc47457e1537b58ea1c9e44382a3
Scotty Vernon <scott.vernon@gmail.com>
2016-04-30 17:30:14 +0100

    Tweaks to brew cask.

---
+++ brew-cask.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+# brew tap caskroom/versions
+
+# daily
+brew cask install spectacle #window resizing
+# brew cask install dropbox
+brew cask install gyazo #screenshot library
+# brew cask install 1password
+brew cask install rescuetime #records habits
+brew cask install flux #screen tempterature
+
+# dev
+# brew cask install iterm2
+# brew cask install sublime-text
+brew cask install atom
+brew cask install imagealpha
+brew cask install imageoptim
+
+# fun
+# brew cask install limechat
+brew cask install miro-video-converter
+
+# browsers
+brew cask install google-chrome-canary
+# brew cask install firefoxnightly
+# brew cask install webkit-nightly
+# brew cask install chromium
+# brew cask install torbrowser
+
+# less often
+brew cask install screenflow
+# brew cask install vlc
+brew cask install licecap
+
+
+# Not on cask but I want regardless.
+
+# File Multi Tool 5
