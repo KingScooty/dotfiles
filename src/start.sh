fd31f05a8314e5f5ca877e52198ce24e5eeb2500
Scotty Vernon <scott.vernon@gmail.com>
2017-12-17 16:34:58 +0000

    Wire up tests to start script

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,35 @@
-# Install deps
-source brew.sh
-source brew-cash.sh
-source npm.sh
-source zsh.sh
 
-source bootstrap.sh
+main() {
+
+    # Ensure that the following actions
+    # are made relative to this file's path.
+
+    cd "$(dirname "${BASH_SOURCE[0]}")" \
+        || exit 1
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Load utils
+
+    if [ -x "utils.sh" ]; then
+        . "utils.sh" || exit 1
+    else
+        echo "ERROR WITH UTILS"
+        exit 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    skip_questions "$@" \
+        && skipQuestions=true
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+    # Install deps
+    ./brew.sh
+    ./brew-cash.sh
+    ./npm.sh
+    ./zsh.sh
+
+    ./bootstrap.sh
